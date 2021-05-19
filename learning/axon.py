b8d31d822719fee08df9bde7bbd16d0cd0ddcf24
unknown <123>
2021-05-19 23:39:00 +0800

    calibration calculate the position of camera succ

--- learning/axon.py
+++ learning/axon.py
@@ -1,4 +1,5 @@
 import numpy as np
+from calib_axon import calc_image_points, get_world_pts_to_obj_coord
 import video_manager
 import os
 from PIL import Image
@@ -132,24 +133,98 @@ def infer_net(net, depth_image):
     return param
 
 
-def ir_camera_calibration():
+def ir_camera_calibration_legacy():
     cam = video_manager.video_manager()
 
     import matplotlib.pyplot as plt
     import cv2
-    from axon_calibrate import get_camera_pts_to_world_coord
+    from calib_axon import get_camera_pts_to_world_coord
     plt.ion()
     fig1 = plt.figure('frame')
-
+    iter = 0
     while True:
         # clear but do not close the figure
         fig1.clf()
         ax1 = fig1.add_subplot(1, 1, 1)
         image = get_ir_image(cam).astype(np.uint8)
-        print(image.shape)
+        from PIL import Image
+        new_pil_image = Image.fromarray(image)
+        new_pil_image.save(f"tmp/{iter}.png")
+        iter += 1
+        # print(image.shape)
         ax1.imshow(image)
-        # get_camera_pts_to_world_coord([image])
+        mat_lst = get_camera_pts_to_world_coord([image], False)
+        print(f"camera pos {mat_lst[0][:, 3]}")
+        # draw the image
+        ax1.title.set_text("ir image")
 
+        # pause
+        plt.pause(3e-2)
+
+    print(f"get ir succ, shape {image.shape}")
+
+
+def ir_camera_calibration():
+    cam = video_manager.video_manager()
+
+    import matplotlib.pyplot as plt
+    import cv2
+    # from calib_axon import get_camera_pts_to_world_coord
+    from calib_axon import calibrate_camera_extrinstic, get_mtx_and_dist, convert_rtvecs_to_transform
+    mtx, dist = get_mtx_and_dist()
+
+    plt.ion()
+    fig1 = plt.figure('frame')
+    iter = 0
+    minus_id = 0
+    positive_id = 0
+    # os.makedirs("positive")
+    # os.makedirs("negative")
+    while True:
+        # print("------------------------")
+        # clear but do not close the figure
+        fig1.clf()
+        ax1 = fig1.add_subplot(1, 1, 1)
+        image = get_ir_image(cam).astype(np.uint8)
+        # from PIL import Image
+        # new_pil_image = Image.fromarray(image)
+        # new_pil_image.save(f"tmp/{iter}.png")
+        iter += 1
+        # print(image.shape)
+        ax1.imshow(image)
+        # mat_lst = get_camera_pts_to_world_coord([image], False)
+        image = np.ascontiguousarray(image, dtype=np.uint8)
+
+        # print(f"image shape {image.shape}")
+        for i in range(image.shape[0]):
+            for j in range(image.shape[1]):
+                if i < 50 and j < 100:
+                    image[i, j] = 0
+        rvecs, tvecs, X_plus_vector_in_screen_coords, Y_plus_vector_in_screen_coords = calibrate_camera_extrinstic(
+            mtx, dist, [image])
+        
+        # print(
+        #     f"X plus {X_plus_vector_in_screen_coords}, Y plus {Y_plus_vector_in_screen_coords}"
+        # )
+        if rvecs is not None and tvecs is not None:
+            obj_pts_to_camera_coords = convert_rtvecs_to_transform(
+                rvecs, tvecs)
+            # print(f"obj_pts_to_camera_coords\n {obj_pts_to_camera_coords[0]}")
+            world_pts_to_camera_coords = np.matmul(
+                obj_pts_to_camera_coords[0],
+                get_world_pts_to_obj_coord(X_plus_vector_in_screen_coords,
+                                           Y_plus_vector_in_screen_coords))
+            camera_pts_to_world_coords = np.linalg.inv(
+                world_pts_to_camera_coords)
+            print(f"camera pos {camera_pts_to_world_coords[:, 3]}")
+            # if camera_pts_to_world_coords[0, 3] < 0:
+            #     Image.fromarray(image).save(f"negative/{minus_id}.bmp")
+            #     minus_id += 1
+            # else:
+            #     Image.fromarray(image).save(f"positive/{positive_id}.bmp")
+            #     positive_id += 1
+        else:
+            print("[warn] calibrate failed")
         # draw the image
         ax1.title.set_text("ir image")
 
@@ -160,7 +235,25 @@ def ir_camera_calibration():
 
 
 if __name__ == "__main__":
+    np.set_printoptions(suppress=True)
     ir_camera_calibration()
+    # import cv2
+    # from calib_axon import calc_objp, calc_objective_coordinate_in_screen_coordinate
+    # img = cv2.imread("images/1.bmp")
+    # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # objp = calc_objp()
+    # objpoints, imgpoints = calc_image_points([img], objp)
+    # print(f"obj points {objpoints}")
+    # print(f"img points {imgpoints}")
+    # X_positive, Y_positive = calc_objective_coordinate_in_screen_coordinate(
+    #     objpoints[0], imgpoints[0])
+
+    # new_objp = objp[0]
+    # new_objp[]
+
+    # chess_board_size = get_chessboard_size()
+
+    # ir_camera_calibration()
     # cam = video_manager.video_manager()
     # unit = cam.GetDepthUnit_mm()
     # print(f"cur unit {unit} mm")
