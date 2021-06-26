4e168da1c55d0b44e5135b8683799543a3335612
xudong <xudongfeng1996@gmail.com>
2021-06-26 21:50:34 +0800

    update scripts

--- scripts/gen_isotropic_sampling_config.py
+++ scripts/gen_isotropic_sampling_config.py
@@ -19,8 +19,8 @@ class cPropertyManager:
     def dump_to_json(self, path):
         prop_name_list = [i for i in self.property_dict]
         value = {
-            "prop_name_list" : prop_name_list,
-            "prop_samples" : self.property_sample_lst
+            "prop_name_list": prop_name_list,
+            "prop_samples": self.property_sample_lst
         }
         with open(path, 'w') as f:
             json.dump(value, f)
@@ -31,21 +31,29 @@ class cPropertyManager:
         self.property_sample_lst.append(prop)
 
 
+def gen_isotropic_config(manager, samples, path):
+    bending_begin = 0
+    bending_end = 50
+    samples = 200
+    stretch = 27
+    for i in range(samples):
+        cur_bending = bending_begin + (bending_end -
+                                       bending_begin) / (samples - 1) * i
+        mana.add(
+            [stretch, stretch, stretch, cur_bending, cur_bending, cur_bending])
+
+    mana.dump_to_json(
+        "../config/train_configs/isotropic_properties_sample_200.json")
+
+
 if __name__ == "__main__":
     mana = cPropertyManager()
     mana.register("stretch_warp", 0)
     mana.register("stretch_weft", 1)
-    mana.register("bending_warp", 2)
-    mana.register("bending_weft", 3)
-    mana.register("bending_bias", 4)
+    mana.register("stretch_bias", 2)
+    mana.register("bending_warp", 3)
+    mana.register("bending_weft", 4)
+    mana.register("bending_bias", 5)
 
-    bending_begin = 5e2
-    bending_end = 5e3
-    samples = 200
-    stretch = 1e5
-    for i in range(samples):
-        cur_bending = bending_begin + (bending_end -
-                                       bending_begin) / (samples - 1) * i
-        mana.add([stretch, stretch, cur_bending, cur_bending, cur_bending])
-
-    mana.dump_to_json("../config/train_configs/isotropic_properties_sample_200.json")
+    # for isotropi config
+    # gen_isotropic_config()
--- src/scenes/SynDataScene.cpp
+++ src/scenes/SynDataScene.cpp
@@ -244,7 +244,9 @@ void cSynDataScene::OfflineSampling()
     int num_of_properties = mPropManager->GetNumOfProperties();
     int num_of_samples = num_of_properties;
     num_of_samples *= this->mSynDataNoise->mNumOfSamplesPerProp;
-    printf("[log] we will have %d samples\n", num_of_samples);
+    float time_cost_per_sample = 16; // s
+    float total_time_day = time_cost_per_sample * num_of_samples / (3600 * 24);
+    printf("[log] we will have %d samples, nearly cost %.1f day\n", num_of_samples, total_time_day);
     for (int i = 0; i < num_of_properties; i++)
     {
         // 1. reset the internal linctex scene,
